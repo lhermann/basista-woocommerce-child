60e9cff2a820d0b261ddbd6160b99c67fe6b2b95
Lukas Hermann <lukher6@web.de>
2018-12-04 11:50:12 +0800

    Remove sidebar on product page

--- functions.php
+++ functions.php
@@ -62,11 +62,28 @@ function register_styles_and_scrips() {
 add_action( 'wp_enqueue_scripts', 'register_styles_and_scrips' );
 
 
+/**
+ * Add book author under product title
+ */
 function add_book_author($arg1) {
     global $product;
     $author = $product->get_attribute('pa_autor');
     if($author) echo "<p class=\"basista-loop-product__author\">$author</p>";
 }
-
 add_action( 'woocommerce_shop_loop_item_title', 'add_book_author');
-// woocommerce_template_loop_product_title
+
+
+/**
+ * @snippet       Remove Sidebar @ Single Product Page for Storefront Theme
+ * @how-to        Watch tutorial @ https://businessbloomer.com/?p=19055
+ * @sourcecode    https://businessbloomer.com/?p=19572
+ * @author        Rodolfo Melogli
+ * @testedwith    WooCommerce 3.2.6
+ */
+
+function remove_storefront_sidebar() {
+    if ( is_product() ) {
+        remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
+    }
+}
+add_action( 'get_header', 'remove_storefront_sidebar' );
